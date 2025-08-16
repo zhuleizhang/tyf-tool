418186147b713dafc20886b0e38db120cdb432f1
Coda <coda@bytedance.com>
2025-08-16 17:36:53 +0800

    (By [Coda] )

--- src/main.ts
+++ src/main.ts
@@ -42,8 +42,9 @@ function createWindow() {
 		width: 1200,
 		height: 800,
 		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
+			nodeIntegration: false,
+			contextIsolation: true,
+			preload: path.join(__dirname, 'preload.js'),
 		},
 	});
 
--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,10 @@ module.exports = {
 					from: path.resolve(__dirname, 'index.html'),
 					to: path.resolve(__dirname, 'dist'),
 				},
+				{
+					from: path.resolve(__dirname, 'preload.js'),
+					to: path.resolve(__dirname, 'dist'),
+				},
 			],
 		}),
 	],
